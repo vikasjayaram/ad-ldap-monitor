8cba94d378c3a01c99c1d3eb456699a685606005
Vikas Kannurpatti Jayaram <vikas@auth0.com>
2016-06-29 11:47:06 +1000

    Check for authentication every 30 minutes
    Graceful stop the timers before re-initialising.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Monitor = require('./lib/monitor');
 const connectors = require('./connectors');
 const http = require('http');
 const port = process.env.PORT || 3008;
-const connectors_list = [], monitors = [];
 const logger = require('./logger');
+let connectors_list = [], monitors = [];
 
 let server = null;
 // Read the config.
@@ -16,37 +16,48 @@ nconf
   .argv()
   .env();
 
-authenticate(function(err, token) {
-  console.log(token);
-  if (err) {
-    logger.error('Error authenticating:', err.message);
-
-  }
-
-  connectors.forEach(function (connector) {
-    if (connector.timeout != undefined && connector.timeout > 0) {
-      var monitor_connector = new Monitor ({
-          token: token,
-          connection: connector.name,
-          timeout: connector.timeout
-      });
-
-      connectors_list.push(connector.name);
-      monitors.push(monitor_connector);
-    } else {
-      logger.error('The timeout value should be defined and must be greater than 0');
-      process.exit(1);
+const monitor_connectors = function () {
+  authenticate(function(err, token) {
+    if (err) {
+      logger.error('Error authenticating:', err.message);
     }
-  });
-});
+    if (monitors.length > 0) {
+      monitors.forEach(function (monitor) {
+        monitor.stop();
+      });
+    }
+    connectors_list = [], monitors = [];
+    connectors.forEach(function (connector) {
+      if (connector.timeout != undefined && connector.timeout > 0) {
+        var monitor_connector = new Monitor ({
+            token: token,
+            connection: connector.name,
+            timeout: connector.timeout
+        });
 
+        connectors_list.push(connector.name);
+        monitors.push(monitor_connector);
+      } else {
+        logger.error('The timeout value should be defined and must be greater than 0');
+        process.exit(1);
+      }
+    });
+    return next();
+  });
+}
+
+const next = function() {
+  // Run every 30 minutes
+  setTimeout(monitor_connectors, 30000);
+}
 
 server = http.createServer(function (req, res) {
     var data = "Monitoring the following connectors: \n \n" + connectors_list.join("\n");
-
     res.end(data);
 });
 
 
+
 server.listen(port);
+monitor_connectors();
 console.log('Listening to port %s', port);
